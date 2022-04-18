560537d9ceb72580b6e2e251befe2f817e2859c4
hgsayim <hgsayim@gmail.com>
2022-04-18 16:53:58 +0300

    initialize

---
+++ app/src/main/java/com/covid19riskmeter/MainActivity.java
@@ -0,0 +1,22 @@
+package com.covid19riskmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnMoveToRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void moveToRegisterPage(View view){
+        Intent i = new Intent(this,RegisterActivity.class);
+        startActivity(i);
+    }
+
+}
